993457cb9422a7315c8f0db6666d2c5349de0283
Parth Pandey <parthpandey2611@gmail.com>
2022-08-24 23:51:56 +0530

    Create Palindrome Linked List.java

---
+++ Palindrome Linked List.java
@@ -0,0 +1,36 @@
+// https://leetcode.com/problems/palindrome-linked-list/
+
+class Solution {
+    public boolean isPalindrome(ListNode head) {
+        ListNode fast = head;
+        ListNode slow = head;
+        while(fast!=null && fast.next!=null){
+            fast=fast.next.next;
+            slow=slow.next;
+       } 
+    slow=reverse(slow);
+    fast=head;
+ 
+// compare both halves
+    while(slow!=null){
+        if(fast.val!=slow.val){
+            return false ;
+        }
+        slow=slow.next;
+        fast=fast.next;
+    }
+       return true ; 
+ } 
+ 
+// this code is used to reverse the LinkedList  
+    public ListNode reverse(ListNode head){
+        ListNode prev = null ;
+        while(head!=null){
+                ListNode temp = head.next;
+                head.next=prev;
+                prev=head;
+                head=temp; 
+        }
+        return prev;
+    }
+}
