a2eb983d9e3cd47a3eb78c4ba93b174119998855
Parth Pandey <parthpandey2611@gmail.com>
2022-08-25 15:18:40 +0530

    Update Power of Three.java

--- Power of Three.java
+++ Power of Three.java
@@ -14,3 +14,9 @@ class Solution {
         return false ;
     }
 }
+
+// Another approach ->
+//     public boolean isPowerOfFour(int n) {
+//       if(n>1) while(n%3==0) n /= 3;
+//       return n==1;    
+//     }
