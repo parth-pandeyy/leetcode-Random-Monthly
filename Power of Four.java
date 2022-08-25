ff0b00a41adf84bad3bf0f5d6f8459c23f918eaa
Parth Pandey <parthpandey2611@gmail.com>
2022-08-25 15:17:48 +0530

    Update Power of Four.java

--- Power of Four.java
+++ Power of Four.java
@@ -14,3 +14,9 @@ class Solution {
         return false;
     }
 }
+
+// Another approach ->
+//     public boolean isPowerOfFour(int n) {
+//       if(n>1) while(n%4==0) n /= 4;
+//       return n==1;    
+//     }
