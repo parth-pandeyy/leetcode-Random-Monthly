7eacc2957e265299ee79a1cbb9cc91d3c3098c8c
Parth Pandey <parthpandey2611@gmail.com>
2022-09-07 10:47:54 +0530

    Create Vertical Traversal of Binary Tree.java

---
+++ Vertical Traversal of Binary Tree.java
@@ -0,0 +1,46 @@
+//https://practice.geeksforgeeks.org/problems/print-a-binary-tree-in-vertical-order/1
+
+class Solution
+{
+    //Function to find the vertical order traversal of Binary Tree.
+    static ArrayList <Integer> verticalOrder(Node root)
+    {
+        Queue<Pair> q = new ArrayDeque<>();
+        Map<Integer , ArrayList<Integer>> map = new TreeMap<>();
+        q.add(new Pair(0,root));  // creating new pair and then adding root in it. Pair is used to access                                        left and right children of nodes
+        while(!q.isEmpty()){
+  // here you are entering in the map
+            Pair cur = q.poll();       // cur-> current 
+            if(map.containsKey(cur.hd)){
+                map.get(cur.hd).add(cur.node.data);
+            }
+            else{
+                ArrayList<Integer> temp = new ArrayList<>();
+                temp.add(cur.node.data);
+                map.put(cur.hd,temp);
+            }
+  // this part is used to maintain the level order traversal
+            if(cur.node.left!=null){
+                q.add(new Pair(cur.hd-1,cur.node.left));
+            }
+            if(cur.node.right!=null){
+                q.add(new Pair(cur.hd+1,cur.node.right));
+            }
+        }
+        // so to convert map into List , we have to do traversing inside the map
+        ArrayList<Integer> ans = new ArrayList<>();
+        for(Map.Entry<Integer,ArrayList<Integer>> entry:map.entrySet()){
+            ans.addAll(entry.getValue());
+        }
+    return ans;
+}
+    static class Pair{
+        // Making pair of hd & node
+        int hd ; // horizontal distance 
+        Node node ;  // node 
+    public Pair(int hd ,Node node){
+        this.node = node;
+        this.hd = hd ;
+    }
+  }
+}
