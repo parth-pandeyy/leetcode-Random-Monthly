461c9df5d181de31b0644b76f5206b19dc36e4a7
Parth Pandey <parthpandey2611@gmail.com>
2022-09-07 22:50:24 +0530

    Update Vertical Traversal of Binary Tree.java

--- Vertical Traversal of Binary Tree.java
+++ Vertical Traversal of Binary Tree.java
@@ -1,4 +1,58 @@
-//https://practice.geeksforgeeks.org/problems/print-a-binary-tree-in-vertical-order/1
+// https://leetcode.com/problems/vertical-order-traversal-of-a-binary-tree/
+// Approach -> https://leetcode.com/problems/vertical-order-traversal-of-a-binary-tree/discuss/231140/Solved-Add-clarification-for-the-output-order
+//             https://www.youtube.com/watch?v=q_a6lpbKJdw&t=947s
+
+class Solution {
+        int min = 0 , max = 0 ;
+        Map<Integer , List<Integer>> map = new HashMap();
+        public List<List<Integer>> verticalTraversal(TreeNode root) {
+        List<List<Integer>> res = new ArrayList();
+        if(root == null){
+            return res;
+        }
+        Queue<TreeNode> qt = new LinkedList(); 
+        Queue<Integer> qi = new LinkedList();
+        qt.add(root);
+        qi.add(0);
+        while(!qt.isEmpty()){
+            int size = qt.size();
+            Map<Integer , List<Integer>> temp = new HashMap();
+            for(int i =0 ; i<size ; i++){
+                TreeNode cur = qt.poll();
+                int idx = qi.poll();
+                if(!temp.containsKey(idx)){
+                    temp.put(idx , new ArrayList<Integer>());
+                }
+                temp.get(idx).add(cur.val);
+                if(idx<min) min=idx;
+                if(idx>max) max=idx;
+                if(cur.left!=null){
+                   qt.add(cur.left);
+                   qi.add(idx-1);
+                }
+                if(cur.right!=null){
+                    qt.add(cur.right);
+                    qi.add(idx+1);
+                }
+            }
+          for(int key : temp.keySet()){
+                if(!map.containsKey(key)) map.put(key , new ArrayList<Integer>());
+                List<Integer> list = temp.get(key);
+                Collections.sort(list);
+                map.get(key).addAll(list);     
+        }
+    }
+          for(int i =min ; i<=max ; i++){
+              List<Integer> list = map.get(i);
+              res.add(list);
+          }
+            return res ;
+    }
+}
+_________________________________________________________________________________________________________________________________________________________
+
+// https://practice.geeksforgeeks.org/problems/print-a-binary-tree-in-vertical-order/1
+// Approach -> https://www.youtube.com/watch?v=h7xALnzllec&t=478s
 
 class Solution
 {
@@ -7,7 +61,7 @@ class Solution
     {
         Queue<Pair> q = new ArrayDeque<>();
         Map<Integer , ArrayList<Integer>> map = new TreeMap<>();
-        q.add(new Pair(0,root));  // creating new pair and then adding root in it. Pair is used to access                                        left and right children of nodes
+        q.add(new Pair(0,root));  // creating new pair and then adding root in it. Pair is used to access left and right children of nodes
         while(!q.isEmpty()){
   // here you are entering in the map
             Pair cur = q.poll();       // cur-> current 
