f24d8e92d6a5cac92493592cf8032753efc1c620
Parth Pandey <parthpandey2611@gmail.com>
2022-12-15 23:12:44 +0530

    Time: 0 ms (100.00%), Space: 40 MB (81.15%) - LeetHub

--- 0198-house-robber/0198-house-robber.java
+++ 0198-house-robber/0198-house-robber.java
@@ -1,35 +1,36 @@
-// class Solution {
-//     public int rob(int[] nums) {
-//         int a=0;
-//         int b=0;
-//         for (int i=0; i<nums.length; i++) {
-//             if(i%2 == 0) a+=nums[i];
-//             else b+=nums[i];
-      
-//         }
-//             return Math.max(a,b);
-//     }
-// }
-
-
-
 class Solution {
     public int rob(int[] nums) {
-   int a = 0;
-    int b = 0;
-    
-    for (int i=0; i<nums.length; i++)
-    {
-        if (i%2==0)
-        {
-            a = Math.max(a+nums[i], b);
-        }
-        else
-        {
-            b = Math.max(a, b+nums[i]);
-        }
+    if (nums.length == 1) return nums[0];
+    if (nums.length < 1) return 0;
+    int[] dp = new int[nums.length];
+    dp[0] = nums[0];
+    dp[1] = Math.max(nums[0],nums[1]);
+    for (int i = 2; i<nums.length; i++) {
+        dp[i] = Math.max(dp[i-2]+nums[i], dp[i-1]);
     }
-    
-    return Math.max(a, b);
-    }
+    return dp[nums.length-1];
+  }
 }
+
+
+
+// class Solution {
+//     public int rob(int[] nums) {
+//    int a = 0;
+//     int b = 0;
+    
+//     for (int i=0; i<nums.length; i++)
+//     {
+//         if (i%2==0)
+//         {
+//             a = Math.max(a+nums[i], b);
+//         }
+//         else
+//         {
+//             b = Math.max(a, b+nums[i]);
+//         }
+//     }
+    
+//     return Math.max(a, b);
+//     }
+// }
