fe2d244a0567089206abefa4c2bd94507863013b
Parth Pandey <parthpandey2611@gmail.com>
2022-12-15 16:48:29 +0530

    Time: 0 ms (100.00%), Space: 40.1 MB (79.00%) - LeetHub

---
+++ 0198-house-robber/0198-house-robber.java
@@ -0,0 +1,35 @@
+// class Solution {
+//     public int rob(int[] nums) {
+//         int a=0;
+//         int b=0;
+//         for (int i=0; i<nums.length; i++) {
+//             if(i%2 == 0) a+=nums[i];
+//             else b+=nums[i];
+      
+//         }
+//             return Math.max(a,b);
+//     }
+// }
+
+
+
+class Solution {
+    public int rob(int[] nums) {
+   int a = 0;
+    int b = 0;
+    
+    for (int i=0; i<nums.length; i++)
+    {
+        if (i%2==0)
+        {
+            a = Math.max(a+nums[i], b);
+        }
+        else
+        {
+            b = Math.max(a, b+nums[i]);
+        }
+    }
+    
+    return Math.max(a, b);
+    }
+}
