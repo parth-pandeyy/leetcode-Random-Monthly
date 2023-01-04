ae854dc9929495fa370968bd127035c6d12b4f23
Parth Pandey <parthpandey2611@gmail.com>
2023-01-04 18:17:53 +0530

    Time: 8 ms (92.79%), Space: 42.5 MB (75.45%) - LeetHub

--- 0944-delete-columns-to-make-sorted/0944-delete-columns-to-make-sorted.java
+++ 0944-delete-columns-to-make-sorted/0944-delete-columns-to-make-sorted.java
@@ -1,24 +1,16 @@
 class Solution {
-    public int minDeletionSize(String[] strs) {
-        // Initialize the delete count to 0
-        int deleteCount = 0;
-        // Get the number of rows and columns in the grid
-        int n = strs.length;
-        int m = strs[0].length();
-        // Iterate through each column of the grid
-        for (int i = 0; i < m; i++) {
-            // Iterate through each element in the column
-            for (int j = 1; j < n; j++) {
-                // If the current element is lexicographically smaller than the previous element,
-                // increment the delete count and break out of the loop
-                if (strs[j].charAt(i) < strs[j - 1].charAt(i)) {
-                    deleteCount++;
+    public int minDeletionSize(String[] strs){
+        int countDelete=0;
+        int row = strs.length;
+        int col = strs[0].length();
+        for(int i=0 ; i<col ; i++){
+            for(int j=0; j<row-1 ; j++){
+                if(strs[j].charAt(i)>strs[j+1].charAt(i)){
+                    countDelete++;
                     break;
                 }
             }
         }
-        // Return the delete count
-        return deleteCount;
+        return countDelete;
     }
 }
-   
