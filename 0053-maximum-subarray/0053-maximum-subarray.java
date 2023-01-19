df4c0d59888084acc31db342253ddf7bd5758094
Parth Pandey <parthpandey2611@gmail.com>
2023-01-19 14:44:30 +0530

    Time: 1 ms (100.00%), Space: 51.6 MB (73.65%) - LeetHub

---
+++ 0053-maximum-subarray/0053-maximum-subarray.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int maxSubArray(int[] nums) {
+        int total=0, curmax=0 , maxSum=nums[0] , curmin=0 , minSum=nums[0];
+        for(int a : nums){
+          //  total+=a;
+            curmax=Math.max(a,curmax+a);
+            maxSum=Math.max(maxSum,curmax);
+        //    curmin=Math.min(a,curmin+a);
+          //  minSum=Math.min(minSum,curmin);
+        }
+        return maxSum;
+    }
+}
