469d7dfa1546d0ce65a7288e8f769330854be2c1
Parth Pandey <parthpandey2611@gmail.com>
2023-01-24 04:34:11 +0530

    Time: 4 ms (55.24%), Space: 75.2 MB (5.63%) - LeetHub

--- 0997-find-the-town-judge/0997-find-the-town-judge.java
+++ 0997-find-the-town-judge/0997-find-the-town-judge.java
@@ -5,7 +5,7 @@ class Solution {
             count[t[0]]--;
             count[t[1]]++;
         }
-        for(int i=1 ; i<=n ; ++i){
+        for(int i=1 ; i<=n ; i++){
             if(count[i]==n-1) return i;
         }
         return -1;
