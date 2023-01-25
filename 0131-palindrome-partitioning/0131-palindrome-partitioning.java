3b45b811a2d1aa72401b88eaf95c20a7d35b69d1
Parth Pandey <parthpandey2611@gmail.com>
2023-01-25 21:25:06 +0530

    Update 0131-palindrome-partitioning.java

--- 0131-palindrome-partitioning/0131-palindrome-partitioning.java
+++ 0131-palindrome-partitioning/0131-palindrome-partitioning.java
@@ -12,11 +12,11 @@ class Solution {
         for(int i=start ; i<s.length() ; i++){
          // only do backtracking when current string is palindrome
           if(ispalindrome(s,start,i)){
-// if the substring of s is palindrome, we add it into the step, which means we choose this       substring. 
+         // if the substring of s is palindrome, we add it into the step, which means we choose this substring. 
               templist.add(s.substring(start,i+1));
-//we want to do the same thing to the remaining substring.So we recursively call our function.
+         //we want to do the same thing to the remaining substring.So we recursively call our function.
               backtrack(ans,templist,s,i+1);
-// remove the chosen substring, in order to try other options.
+         // remove the chosen substring, in order to try other options.
               templist.remove(templist.size()-1);
             }
         }
