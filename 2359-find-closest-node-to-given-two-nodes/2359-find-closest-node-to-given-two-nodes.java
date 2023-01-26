8d78140ebcbecb27c5f523d69f2be1360edcd16d
Parth Pandey <parthpandey2611@gmail.com>
2023-01-27 02:59:30 +0530

    Update 2359-find-closest-node-to-given-two-nodes.java

--- 2359-find-closest-node-to-given-two-nodes/2359-find-closest-node-to-given-two-nodes.java
+++ 2359-find-closest-node-to-given-two-nodes/2359-find-closest-node-to-given-two-nodes.java
@@ -1,3 +1,21 @@
+// https://leetcode.com/problems/find-closest-node-to-given-two-nodes/discuss/3095888/Day-25-oror-Graph-DFS-oror-Easiest-Beginner-Friendly-Sol-oror-O(n)-time-and-O(n)-space
+// https://www.youtube.com/watch?v=S0jZu7H7nQ0
+
+--------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+// Approach:
+
+//1) Initialize variables n to the size of the edges array, ans to -1, and minDist to the maximum value of an int.
+//2) Create two arrays dist1 and dist2 of size n and initialize all elements to 0. Also, create two arrays visited1 and visited2 of size n and initialize all elements to false.
+//3) Run the depth-first search (DFS) algorithm from node1 and update the dist1 and visited1 arrays accordingly.
+//4) Run the DFS algorithm from node2 and update the dist2 and visited2 arrays accordingly.
+//5) Iterate through all nodes currNode in the graph.
+//6) For each node, check if it has been visited by both DFS calls, and if its maximum distance from node1 and node2 (i.e. max(dist1[currNode], dist2[currNode])) is less than the current value of minDist.
+//7) If the above conditions are met, update the value of minDist and ans to the current node's distance and index, respectively.
+//8) Return ans as the result.
+//9) This algorithm finds the closest meeting point between two given nodes in a graph by using DFS to calculate the distance from each node to all other nodes in the graph. 
+//   It only considers nodes that are reachable from both given nodes, and chooses the one that has the smallest maximum distance from the two given nodes.
+--------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
 class Solution {
     // we are using DFS to solve the problem-
     public int closestMeetingNode(int[] edges, int node1, int node2) {
