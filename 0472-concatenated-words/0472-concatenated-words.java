b811a2bdec91898d8f0bf5bd79621d7ceb8efd13
Parth Pandey <parthpandey2611@gmail.com>
2023-01-28 02:42:03 +0530

    Time: 183 ms (10.30%), Space: 108.1 MB (9.70%) - LeetHub

--- 0472-concatenated-words/0472-concatenated-words.java
+++ 0472-concatenated-words/0472-concatenated-words.java
@@ -1,3 +1,14 @@
+//1) Create an empty set 's' to store all the words in the given array of strings.
+//2) Iterate through the array of strings and insert each word into the set 's'.
+//3) Create an empty vector 'concatenateWords' to store all the concatenated words.
+//4) Iterate through the array of strings again, for each word, check if it is a concatenated      word using the function 'checkConcatenate(word)'.
+//5) In the 'checkConcatenate(word)' function, use a for loop to iterate through each              substring of the word, starting from index 1 to the second last index of the word.
+//6) For each substring, check if the prefix and suffix of the substring exists in the set          's'.
+//7) If the prefix and suffix both exist in the set 's', then return true, indicating that the      word is a concatenated word.
+//8) If the function 'checkConcatenate(word)' returns true, then insert the word into the          'concatenateWords' vector.
+//9) Return the 'concatenateWords' vector.
+
+
 class Solution {
     public List<String> findAllConcatenatedWordsInADict(String[] words) {
         Set<String> s = new HashSet<>();
