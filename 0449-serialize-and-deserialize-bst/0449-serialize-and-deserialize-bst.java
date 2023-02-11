e9d6a77bc965d47c15966afb33f5a889c818af8e
Parth Pandey <parthpandey2611@gmail.com>
2023-02-12 01:44:16 +0530

    Time: 15 ms (35.86%), Space: 43.4 MB (59.76%) - LeetHub

--- 0449-serialize-and-deserialize-bst/0449-serialize-and-deserialize-bst.java
+++ 0449-serialize-and-deserialize-bst/0449-serialize-and-deserialize-bst.java
@@ -11,17 +11,17 @@ public class Codec {
 
     // Encodes a tree to a single string.
     public String serialize(TreeNode root) {
-         if(root==null) return null;
+        if(root==null) return null;
         Queue<TreeNode> q = new LinkedList<>();
         StringBuilder res = new StringBuilder();
         q.add(root);
         while(!q.isEmpty()){
             TreeNode node = q.poll();
             if(node==null){
-                res.append("n ");//if node is found means it is null then insert null there
+                res.append("n ");
                 continue;
             }
-            res.append(node.val+" "); // otherwise insert node value
+            res.append(node.val+" ");
             q.add(node.left);
             q.add(node.right);
         }
@@ -30,7 +30,7 @@ public class Codec {
 
     // Decodes your encoded data to tree.
     public TreeNode deserialize(String data) {
-        if(data == null || data == "") return null;
+        if(data==null || data=="") return null;
         Queue<TreeNode> q = new LinkedList<>();
         String[] values = data.split(" ");
         TreeNode root = new TreeNode(Integer.parseInt(values[0]));
@@ -39,9 +39,9 @@ public class Codec {
         while(!q.isEmpty()){
             TreeNode parent = q.poll();
             if(!values[i].equals("n")){
-               TreeNode left = new TreeNode(Integer.parseInt(values[i]));
-               parent.left=left;
-               q.add(left);
+                TreeNode left = new TreeNode(Integer.parseInt(values[i]));
+                parent.left=left;
+                q.add(left);
             }
             if(!values[++i].equals("n")){
                 TreeNode right = new TreeNode(Integer.parseInt(values[i]));
