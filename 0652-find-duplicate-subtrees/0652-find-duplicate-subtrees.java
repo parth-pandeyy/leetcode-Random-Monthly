2871d163275d621e15baa9ed59806af62fbabeaa
Parth Pandey <parthpandey2611@gmail.com>
2023-02-14 02:52:18 +0530

    Time: 18 ms (82.92%), Space: 45.4 MB (87.17%) - LeetHub

--- 0652-find-duplicate-subtrees/0652-find-duplicate-subtrees.java
+++ 0652-find-duplicate-subtrees/0652-find-duplicate-subtrees.java
@@ -13,47 +13,24 @@
  *     }
  * }
  */
-// class Solution {
-//     public List<TreeNode> findDuplicateSubtrees(TreeNode root) {
-//         List<TreeNode> res = new LinkedList<>();
-//         postorder(root,new HashMap<>(),res);
-//         return res;
-//     }
-//     public String postorder(TreeNode node, Map<String,Integer> count , List<TreeNode> res){
-//         if(node==null) return"";
+class Solution {
+    public List<TreeNode> findDuplicateSubtrees(TreeNode root) {
+        List<TreeNode> res = new LinkedList<>();
+        postorder(root,new HashMap<>(),res);
+        return res;
+    }
+    public String postorder(TreeNode node, Map<String,Integer> count , List<TreeNode> res){
+        if(node==null) return "";
        
-//         String representation = postorder(node.left,count,res)+","+node.val+","+postorder(node.right,count,res);
+        String representation = node.val+","+postorder(node.left,count,res)+","+postorder(node.right,count,res);
        
-//         count.put(representation,count.getOrDefault(representation,0)+1);
+        count.put(representation,count.getOrDefault(representation,0)+1);
        
-//         if(count.get(representation)==2) res.add(node);
-//         return representation;
-//     }
-// }
+        if(count.get(representation)==2) res.add(node);
+        return representation;
+    }
+}
 
 
 // Time complexity: O(2n)
 // Space complexity: 0(2n)
-
-
-
-
-
-
-
-
-class Solution {
-public List<TreeNode> findDuplicateSubtrees(TreeNode root) {
-    List<TreeNode> res = new LinkedList<>();
-    postorder(root, new HashMap<>(), res);
-    return res;
-}
-
-public String postorder(TreeNode cur, Map<String, Integer> map, List<TreeNode> res) {
-    if (cur == null) return "#";  
-    String serial = cur.val + "," + postorder(cur.left, map, res) + "," + postorder(cur.right, map, res);
-    map.put(serial, map.getOrDefault(serial, 0) + 1);
-    if (map.get(serial) == 2) res.add(cur);
-    return serial;
-}
-}
