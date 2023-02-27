086e61315abd5a0d48521beb802780418c1a2310
Parth Pandey <parthpandey2611@gmail.com>
2023-02-27 21:05:04 +0530

    Update 0701-insert-into-a-binary-search-tree.java

--- 0701-insert-into-a-binary-search-tree/0701-insert-into-a-binary-search-tree.java
+++ 0701-insert-into-a-binary-search-tree/0701-insert-into-a-binary-search-tree.java
@@ -13,14 +13,21 @@
  *     }
  * }
  */
-// class Solution {
-//     public TreeNode insertIntoBST(TreeNode root, int val){
-//         if(root==null) return new TreeNode(val);
-//         if(root.val<val) root.right=insertIntoBST(root.right,val);
-//         else root.left=insertIntoBST(root.left,val);
-//         return root;
-//     }
-// }
+
+// Recursive Solution
+
+ class Solution {
+    public TreeNode insertIntoBST(TreeNode root, int val){
+         if(root==null) return new TreeNode(val);
+         if(root.val<val) root.right=insertIntoBST(root.right,val);
+         else root.left=insertIntoBST(root.left,val);
+         return root;
+     }
+}
+
+--------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+//Iterative Method
 
 class Solution {
     public TreeNode insertIntoBST(TreeNode root, int val){
