844d5a7668cfe352de242b8bb1314a614a5c6331
Parth Pandey <parthpandey2611@gmail.com>
2023-04-11 15:52:29 +0530

    Time: 1 ms (92.28%), Space: 43.6 MB (93.80%) - LeetHub

--- 0124-binary-tree-maximum-path-sum/0124-binary-tree-maximum-path-sum.java
+++ 0124-binary-tree-maximum-path-sum/0124-binary-tree-maximum-path-sum.java
@@ -14,16 +14,19 @@
  * }
  */
 class Solution {
-    int max = Integer.MIN_VALUE;
-    public int maxPathSum(TreeNode root) {
-        maxDepth(root);
-        return max;
+  public int maxPathSum(TreeNode root) {
+        int[] max = new int[1];
+        max[0] = Integer.MIN_VALUE;
+        maxDepth(root,max);
+        return max[0];
     }
-    public int maxDepth(TreeNode node){
-        if(node==null) return 0 ;
-        int left = Math.max(maxDepth(node.left),0);
-        int right = Math.max(maxDepth(node.right),0);
-        max = Math.max(max,node.val+left+right);
-        return Math.max(right,left)+node.val;
+    
+    
+    public int maxDepth(TreeNode node, int[] max){
+        if(node==null) return 0;
+        int left=Math.max(maxDepth(node.left,max),0);
+        int right=Math.max(maxDepth(node.right,max),0);
+        max[0]= Math.max(max[0],left+right+node.val);
+        return Math.max(left,right)+node.val;
     }
 }
