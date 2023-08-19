e1ade1b97902921c97dfbcce42f6d8ae29b26af3
Parth Pandey <parthpandey2611@gmail.com>
2023-08-20 01:42:50 +0530

    Time: 7 ms (100.00%) | Memory: 44.4 MB (100.00%) - LeetSync

--- 3018-make-string-a-subsequence-using-cyclic-increments/make-string-a-subsequence-using-cyclic-increments.java
+++ 3018-make-string-a-subsequence-using-cyclic-increments/make-string-a-subsequence-using-cyclic-increments.java
@@ -1,33 +1,15 @@
-// class Solution {
-//     public boolean canMakeSubsequence(String str1, String str2) {
-//         int m = str2.length();
-//         int pointer = 0;
-        
-//         for(int ele : str1.toCharArray()){
-//             if(pointer == m) break;
-//             if(ele == str2.charAt(pointer)) pointer++;
-//             if(ele == str2.charAt(pointer)-1) pointer++;
-//             if(ele =='z' && str2.charAt(pointer)=='a') pointer++;
-//         }
-//         return m==pointer;
-//     }
-// }
-
-public class Solution {
+class Solution {
     public boolean canMakeSubsequence(String str1, String str2) {
-        int pointer = 0;
         int m = str2.length();
+        int pointer = 0;
         
-        for (char ele : str1.toCharArray()) {
-            if (pointer == m)
-                break;
-            if (ele == str2.charAt(pointer))
-                pointer++;
-            else if (ele == str2.charAt(pointer) - 1)
-                pointer++;
-            else if (ele == 'z' && str2.charAt(pointer) == 'a')
-                pointer++;
+        for(int ele : str1.toCharArray()){
+            if(pointer == m) break;
+            if(ele == str2.charAt(pointer)) pointer++;
+            else if(ele == str2.charAt(pointer)-1) pointer++;
+            else if(ele =='z' && str2.charAt(pointer)=='a') pointer++;
         }
-        return (pointer == m);
+        return m==pointer;
     }
 }
+
