a19674f75418a8e28da7cef95bf6f4862328e7d5
Parth Pandey <parthpandey2611@gmail.com>
2023-09-01 01:49:21 +0530

    Time: 1 ms (98.41%) | Memory: 39.4 MB (89.23%) - LeetSync

---
+++ 7-reverse-integer/reverse-integer.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int reverse(int x) {
+        int digit = 0;
+        int reversed = 0;
+        while(x!=0){
+            digit = x % 10;
+            x/=10; 
+            if(reversed>Integer.MAX_VALUE/10 || (reversed==Integer.MAX_VALUE/10 && digit>7)){
+                return 0;
+            }
+            if(reversed<Integer.MIN_VALUE/10 || (reversed==Integer.MIN_VALUE/10 && digit<-8)){
+                return 0;
+            }
+            reversed = reversed*10 + digit;
+        }
+        return reversed;
+    }
+}
+
+
