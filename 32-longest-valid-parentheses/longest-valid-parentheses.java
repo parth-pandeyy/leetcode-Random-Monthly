bf36d1c8323494294d00f97ca6cd8b710f71a007
Parth Pandey <parthpandey2611@gmail.com>
2023-10-07 00:49:15 +0530

    Time: 3 ms (74.04%) | Memory: 43.8 MB (5.48%) - LeetSync

---
+++ 32-longest-valid-parentheses/longest-valid-parentheses.java
@@ -0,0 +1,28 @@
+class Solution {
+    public int longestValidParentheses(String s) {
+        if(s == null || s.length() == 0) return 0;
+
+        Deque<Integer> stack = new ArrayDeque<>();
+
+        int res = 0;
+
+        stack.push(-1);
+
+        for(int i=0 ; i<s.length() ; i++){
+            if(s.charAt(i) == '(') stack.push(i);
+
+            else{
+                stack.pop();
+
+                if(stack.isEmpty()){
+                    stack.push(i);
+                }
+                
+                else{
+                    res = Math.max(res,i-stack.peek());
+                }
+            } 
+        }
+        return res;
+    }
+}
