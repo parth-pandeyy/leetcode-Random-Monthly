d3092b3f4f0e8417247db91c3beacec68704b8c9
Parth Pandey <parthpandey2611@gmail.com>
2023-11-11 01:05:41 +0530

    Time: 4 ms (29.55%) | Memory: 41.8 MB (96.61%) - LeetSync

--- 349-intersection-of-two-arrays/intersection-of-two-arrays.java
+++ 349-intersection-of-two-arrays/intersection-of-two-arrays.java
@@ -1,23 +1,37 @@
 class Solution {
     public int[] intersection(int[] nums1, int[] nums2) {
-        Set<Integer> set1 = new HashSet<>();
-        for (int num : nums1) {
-            set1.add(num);
-        }
-
-        Set<Integer> intersectionSet = new HashSet<>();
-        for (int num : nums2) {
-            if (set1.contains(num)) {
-                intersectionSet.add(num);
+        Arrays.sort(nums2);
+        Set<Integer> set = new HashSet<>();
+        for(int num : nums1){
+            if(binarySearch(nums2, num)){
+                set.add(num);
             }
         }
 
-        int[] result = new int[intersectionSet.size()];
         int i = 0;
-        for (int num : intersectionSet) {
-            result[i++] = num;
+        int[] result = new int[set.size()];
+        for(int num : set){
+            result[i++]=num;
         }
-
         return result;
+    }
+
+    public boolean binarySearch(int[] nums, int target){
+        int start = 0;
+        int end = nums.length-1;
+
+        while(start<=end){
+            int mid = start + (end-start) / 2;
+            if(nums[mid]==target){
+                return true;
+            }
+            else if(nums[mid]>=target){
+                end=mid-1;
+            }
+            else{
+                start=mid+1;
+            }
+        }
+        return false;
     }
 }
