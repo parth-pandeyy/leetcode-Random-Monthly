4437cbf705dd3e5b2d1d35f88aab0e4a71dc00b7
Parth Pandey <parthpandey2611@gmail.com>
2024-01-21 18:44:51 +0530

    Time: 0 ms (100.00%) | Memory: 41.1 MB (23.47%) - LeetSync

---
+++ 198-house-robber/house-robber.java
@@ -0,0 +1,40 @@
+// class Solution {
+//     public int rob(int[] nums) {
+//         if(nums.length==0) return 0;
+//         if(nums.length==1) return nums[1];
+
+//         int maxE = 0;
+//         int maxO = 0;
+
+//         for(int i=0 ; i<nums.length ; i++){
+//             if(i%2==0){
+//                 maxE=Math.max((maxE+nums[i]),maxO);
+//             }
+//             else maxO=Math.max((maxO+nums[i]),maxE);
+//         }
+//         return Math.max(maxE,maxO);
+//     }
+// }
+
+class Solution {
+    public int rob(int[] nums) {
+        if (nums.length == 0) {
+            return 0;
+        } else if (nums.length == 1) {
+            return nums[0];
+        }
+
+        int evenSum = 0;
+        int oddSum = 0;
+
+        for (int i = 0; i < nums.length; i++) {
+            if (i % 2 == 0) {
+                evenSum = Math.max(evenSum + nums[i], oddSum);
+            } else {
+                oddSum = Math.max(oddSum + nums[i], evenSum);
+            }
+        }
+
+        return Math.max(evenSum, oddSum);
+    }
+}
